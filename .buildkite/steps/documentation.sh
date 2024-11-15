559fd6defa1aee9ad54923d3c432b7703b3c8b32
Pete Tomasik <senate6268@gmail.com>
2024-11-15 10:51:40 -0500

    Add annotations on error

--- .buildkite/steps/documentation.sh
+++ .buildkite/steps/documentation.sh
@@ -13,7 +13,8 @@ docs_changes="$?"
 
 if [ "${docs_changes:-0}" -ne 0 ] ; then
 	echo "+++ :bk-status-failed: Documentation changes detected!!!"
-	git status --short
+	git status --short | tee -a git_diff_output
+	printf '```git status --short\n%b\n```' "$(cat git_diff_output)" | buildkite-agent annotate --style info
 fi
 
 exit "$docs_changes"
